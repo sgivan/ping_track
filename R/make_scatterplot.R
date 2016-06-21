0178c16ca3fa947810d41ae853aa315d97963789
BWL Software <f1nst3r@yahoo.com>
2016-06-21 08:52:52 -0500

    Added titles to plots. Plot proportional amount of failures instead of counts.

--- R/make_scatterplot.R
+++ R/make_scatterplot.R
@@ -11,11 +11,11 @@ dates <- lapply(data[1],to_date)
 
 png(filename="Rplot%03d.png",width=1440,heigh=720)
 # plot last hour
-plot(dates[[1]][(length(dates[[1]])-30):length(dates[[1]])],data['Fail'][[1]][(length(dates[[1]])-30):length(dates[[1]])],col="blue",type="p",xlab="Time",ylab="Failures/20 ping attempts")
+plot(main="Failures in Last Hour",dates[[1]][(length(dates[[1]])-30):length(dates[[1]])], data['Fail'][[1]][(length(dates[[1]])-30):length(dates[[1]])]/20, col="blue", type="p", xlab="Time", ylab="Failures/Attempts")
 ## plot last 24 hours
-plot(dates[[1]][(length(dates[[1]])-720):length(dates[[1]])],data['Fail'][[1]][(length(dates[[1]])-720):length(dates[[1]])],col="blue",type="p",xlab="Time",ylab="Failures/20 ping attempts")
+plot(main="Failures in Last 24 Hours",dates[[1]][(length(dates[[1]])-720):length(dates[[1]])],data['Fail'][[1]][(length(dates[[1]])-720):length(dates[[1]])]/20,col="blue",type="p",xlab="Time",ylab="Failures/Attempts")
 # plot last week
-plot(dates[[1]][(length(dates[[1]])-5040):length(dates[[1]])],data['Fail'][[1]][(length(dates[[1]])-5040):length(dates[[1]])],col="blue",type="p",xlab="Date",ylab="Failures/20 ping attempts")
+plot(main="Failures in Last Week",dates[[1]][(length(dates[[1]])-5040):length(dates[[1]])],data['Fail'][[1]][(length(dates[[1]])-5040):length(dates[[1]])]/20,col="blue",type="p",xlab="Date",ylab="Failures/Attempts")
 # plot last month
-plot(dates[[1]][(length(dates[[1]])-21600):length(dates[[1]])],data['Fail'][[1]][(length(dates[[1]])-21600):length(dates[[1]])],col="blue",type="p",xlab="Time",ylab="Failures/20 ping attempts")
+plot(main="Failures in Last Month",dates[[1]][(length(dates[[1]])-21600):length(dates[[1]])],data['Fail'][[1]][(length(dates[[1]])-21600):length(dates[[1]])]/20,col="blue",type="p",xlab="Time",ylab="Failures/Attempts")
 #dev.off()
