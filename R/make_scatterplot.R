52ba9833ae62b4473f9882a73159da85dc31d4cd
BWL Software <f1nst3r@yahoo.com>
2017-01-04 02:17:36 -0600

    Create more plots in make_scatterplot.R

--- R/make_scatterplot.R
+++ R/make_scatterplot.R
@@ -10,12 +10,16 @@ ldate
 dates <- lapply(data[1],to_date)
 
 png(filename="Rplot%03d.png",width=1440,heigh=720)
+par(lab=c(10,5,7));
 # plot last hour
-plot(main="Failures in Last Hour",dates[[1]][(length(dates[[1]])-30):length(dates[[1]])], data['Fail'][[1]][(length(dates[[1]])-30):length(dates[[1]])]/20, col="blue", type="p", xlab="Time", ylab="Failures/Attempts")
+plot(main="Failures in Last Hour",dates[[1]][(length(dates[[1]])-30):length(dates[[1]])], data['Fail'][[1]][(length(dates[[1]])-30):length(dates[[1]])]/20, col="blue", type="p", xlab="Minutes", ylab="Failures/Attempts")
 ## plot last 24 hours
-plot(main="Failures in Last 24 Hours",dates[[1]][(length(dates[[1]])-720):length(dates[[1]])],data['Fail'][[1]][(length(dates[[1]])-720):length(dates[[1]])]/20,col="blue",type="p",xlab="Time",ylab="Failures/Attempts")
+par(lab=c(10,5,7));
+plot(main="Failures in Last 24 Hours",dates[[1]][(length(dates[[1]])-720):length(dates[[1]])],data['Fail'][[1]][(length(dates[[1]])-720):length(dates[[1]])]/20,col="blue",type="p",xlab="Hour",ylab="Failures/Attempts")
 # plot last week
-plot(main="Failures in Last Week",dates[[1]][(length(dates[[1]])-5040):length(dates[[1]])],data['Fail'][[1]][(length(dates[[1]])-5040):length(dates[[1]])]/20,col="blue",type="p",xlab="Date",ylab="Failures/Attempts")
+par(lab=c(10,5,7));
+plot(main="Failures in Last Week",dates[[1]][(length(dates[[1]])-5040):length(dates[[1]])],data['Fail'][[1]][(length(dates[[1]])-5040):length(dates[[1]])]/20,col="blue",type="p",xlab="Day",ylab="Failures/Attempts")
 # plot last month
-plot(main="Failures in Last Month",dates[[1]][(length(dates[[1]])-21600):length(dates[[1]])],data['Fail'][[1]][(length(dates[[1]])-21600):length(dates[[1]])]/20,col="blue",type="p",xlab="Time",ylab="Failures/Attempts")
+par(lab=c(10,5,7));
+plot(main="Failures in Last Month",dates[[1]][(length(dates[[1]])-21600):length(dates[[1]])],data['Fail'][[1]][(length(dates[[1]])-21600):length(dates[[1]])]/20,col="blue",type="p",xlab="Day",ylab="Failures/Attempts")
 #dev.off()
