b261bbd1cc58f8bfd427d56e74bdd3e492e6f5a0
BWL Software <f1nst3r@yahoo.com>
2017-02-15 10:18:17 -0600

    Adding new files

--- www/pingTrack/displayPings/views.py
+++ www/pingTrack/displayPings/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.utils import timezone
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    today_date = timezone.now()
+    template = loader.get_template('polls/index.html')
+    context = {
+            'current_date' : today_date,
+            }
+    return HttpResponse(template.render(context,request))
 
 def testme(request):
     return HttpResponse("<h1>This is a test</h1>")
