2682870e8b806038403a556542a7178a0d12cba1
Scott Givan <sgivan@yahoo.com>
2018-10-13 07:40:40 -0500

    Changed png names. Added png to .gitignore.

--- R/make_STplot.R
+++ R/make_STplot.R
@@ -2,12 +2,16 @@
 
 data <- read.csv(file="speedtest.txt", header=T, stringsAsFactors=F)
 
-timepts <- c(4,8,12,18,24,48,72)
+# 72 hrs = 3 days
+# 168 hrs = 7 days
+# 336 hrs = 14 days
+# 720 hrs = 30 days
+timepts <- c(4,8,12,18,24,48,72,168,336,720)
 
 make_STplot <- function (x,filename="st3") {
-    png(filename=paste0("pingTrack_",filename,".png"),width=1440,heigh=720)
+    png(filename=paste0("plots/pingTrack_",filename,".png"),width=1440,heigh=720)
     par(mar=c(5,5,2,5))
-    with(x, plot(c(1:length(Download))*15/60,Download/1e6,ylim=range(c(Upload,Download))/1e6, type="l", col="red3", mar=c(2,5,2,5), xlab="Hours", ylab=NA))
+    with(x, plot(c(1:length(Download))*15/60,Download/1e6,ylim=range(c(Upload,Download))/1e6, type="l", col="red3", mar=c(2,5,2,5), xlab="Hours Ago", ylab=NA))
     par(new=T)
     with(x, plot(c(1:length(Upload))*15/60,Upload/1e6,ylim=range(c(Upload,Download))/1e6, type="l", col="blue3", xlab=NA, ylab=NA, axes=F))
     mtext(side=3, paste0("Network Speed Test (",date(),")"))
@@ -19,7 +23,10 @@ make_STplot <- function (x,filename="st3") {
     legend("topleft",legend=c("Download (MB)","Upload (MB)","Ping (S)"),lty=1,col=c("red3","blue3","green3"))
     legend("topright",legend=c(paste0("cor(Down v Up): ",sprintf("%.2f",cor(x$Download,x$Upload))),
                                paste0("cor(Down v Ping): ", sprintf("%.2f", cor(x$Download,x$Ping))),
-                               paste0("cor(Up v Ping): ", sprintf("%.2f", cor(x$Upload,x$Ping)))
+                               paste0("cor(Up v Ping): ", sprintf("%.2f", cor(x$Upload,x$Ping))),
+                               paste0("Avg Down: ", sprintf("%.2f", mean(x$Download)/1e6), " MB"),
+                               paste0("Avg Up: ", sprintf("%.2f", mean(x$Upload)/1e6), " MB"),
+                               paste0("Avg Ping: ", sprintf("%.2f", mean(x$Ping), " Sec"))
                                ))
     graphics.off()
 }
